6d9dcd46f1f60005f56d1aee6215e03a1434a054
MrLucjusz <patlew@student.agh.edu.pl>
2022-11-15 09:07:01 +0100

    lab4

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -2,15 +2,28 @@ package agh.ics.oop;
 
 public class OptionsParser {
     public static MoveDirection[] parser(String[] args){
+        //returnStream.of(input)
+        //  .map(instruction -> switch (instruction){
+        //      case "f", "forward" -> news[i] = MoveDirection.FORWARD;
+        //      case "b", "backward" -> news[i] = MoveDirection.BACKWARD;
+        //      case "r", "right" -> news[i] = MoveDirection.RIGHT;
+        //      case "l", "left" -> news[i] = MoveDirection.LEFT;
+        //      default -> null;
+        // }).filter(MoveDirection -> MoveDirection != null)
+        //.toArray(MoveDirection[]::new);
         MoveDirection[] news = new MoveDirection [args.length];
-        for(int i=0; i < args.length; i++){
-            switch (args[i]) {
+        int i = 0;
+        for(String arg: args){
+            news[i] = switch (args[i]) {
                 case "f", "forward" -> news[i] = MoveDirection.FORWARD;
                 case "b", "backward" -> news[i] = MoveDirection.BACKWARD;
                 case "r", "right" -> news[i] = MoveDirection.RIGHT;
                 case "l", "left" -> news[i] = MoveDirection.LEFT;
-            }
+                default -> throw new IllegalArgumentException( "Nieprawidłowa instrukcja!" );
+            };
+            i++;
         }
         return news;
     }
+    //
 }
---
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -0,0 +1,30 @@
+package agh.ics.oop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SimulationEngine implements IEngine{
+
+    private MoveDirection[] directions;
+    private IWorldMap map;
+    private Vector2d[] positions;
+    private List<Animal> animals = new ArrayList<>();
+
+    public SimulationEngine(MoveDirection[] directions, IWorldMap map, Vector2d[] positions){
+        this.directions = directions;
+        this.map = map;
+        this.positions = positions;
+        for (Vector2d position : positions) {
+            Animal animal = new Animal(map, position);
+            map.place(animal);
+            animals.add(animal);
+        }
+    }
+
+    public void run(){
+        for (int i=0; i< directions.length; i++){
+            Animal animal = this.animals.get(i% animals.size());
+            animal.move(directions[i]);
+        }
+    }
+}
